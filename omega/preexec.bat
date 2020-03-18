f16d80a101a579a220e3342b8bf1998421ec939a
Nathaniel Schaaf <schaafna@gmail.com>
2020-03-19 00:15:56 +0100

    Make preexec script log

--- omega/preexec.bat
+++ omega/preexec.bat
@@ -2,6 +2,12 @@ SET appdir=.\path\to\dayz-server-carim
 SET deploydir=.\path\to\DayZServer\deploy
 SET authfile=.\path\to\auth.json
 SET outdir=.\path\to\DayZServer
+SET logfile=.\path\to\preexec.log
 SET "cmd=Python3.exe -m carim.main -d %deploydir% -a %authfile% -o %outdir%"
+call :log >>%logfile% 2>&1
+exit /b
 
-start /d %appdir% cmd /k "%cmd% && exit"
+:log
+cd /d %appdir%
+git pull
+%cmd%
