8ef47f48f3ac22a9757beb8e51a02ad3014db93b
Nathaniel Schaaf <schaafna@gmail.com>
2020-03-20 17:09:34 +0100

    Add code for generating airdrop config

--- carim/configuration/profiles.py
+++ carim/configuration/profiles.py
@@ -30,8 +30,29 @@ def trader_file_and_admins(directory):
 
 @profile(directory='Airdrop')
 def airdrop(directory):
-    p = pathlib.Path('resources/original-mod-files/Airdrop/AirdropSettings.json')
-    file_writing.copy(p, directory)
+    with open('resources/original-mod-files/Airdrop/AirdropSettings.json') as f:
+        airdrop_settings = json.load(f)
+    with open('resources/modifications/airdrop_setting_modifications.json') as f:
+        airdrop_setting_modifications = json.load(f)
+    # http://games.digiacom.com/Airdrop_Server_Guide.pdf
+    for setting in ('Controls', 'Messages', 'Container'):
+        for k, v in airdrop_setting_modifications.get(setting).items():
+            airdrop_settings[setting][k] = v
+
+    drops = []
+    drop_defaults = airdrop_setting_modifications.get('DropZoneDefaults')
+    for drop in airdrop_setting_modifications.get('DropZones'):
+        drops.append({**drop, **drop_defaults})
+    airdrop_settings['DropZones'] = drops
+
+    drop_types = []
+    drop_type_defaults = airdrop_setting_modifications.get('DropTypeDefaults')
+    for drop_type in airdrop_setting_modifications.get('DropTypes'):
+        drop_types.append({**drop_type, **drop_type_defaults})
+    airdrop_settings['DropTypes'] = drop_types
+
+    with file_writing.f_open(pathlib.Path(directory, 'AirdropSettings.json'), mode='w') as f:
+        json.dump(airdrop_settings, f, indent=2)
 
 
 @profile(directory='SimpleBase')
@@ -50,7 +71,6 @@ def simple_base_profile(directory):
         config.set(k, v)
     with file_writing.f_open(pathlib.Path(directory, 'config.txt'), mode='w') as f:
         f.write(config.generate())
-    # destruction config
 
 
 @profile(directory='VPPAdminTools/Permissions/SuperAdmins')
