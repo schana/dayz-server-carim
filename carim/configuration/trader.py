99be747d55884bdc4b83f5556f16818dab6a2650
Nathaniel Schaaf <schaafna@gmail.com>
2020-03-24 13:14:49 +0100

    Remove free green mountain trader config

--- carim/configuration/trader.py
+++ carim/configuration/trader.py
@@ -47,7 +47,6 @@ def trader_items(directory):
             build_category(new_category, category)
 
     add_dynamic(traders)
-    build_green_mountain_free_traders(traders_config)
 
     with file_writing.f_open(pathlib.Path(directory, 'TraderConfig.txt'), mode='w') as f:
         f.write(traders_config.generate())
@@ -139,64 +138,3 @@ def trader_objects_config(directory):
 
     with file_writing.f_open(pathlib.Path(directory, 'TraderObjects.txt'), mode='w') as f:
         f.write(to.generate())
-
-
-def build_green_mountain_free_traders(traders_config):
-    food = trader.Category('Food')
-    weapons = trader.Category('Weapons')
-    explosives = trader.Category('Explosives')
-    clothes = trader.Category('Clothes')
-    containers = trader.Category('Containers')
-    tools = trader.Category('Tools')
-    vehicles = trader.Category('Vehicles')
-
-    for t in types.get().getroot():
-        cat = t.find('category')
-        if cat is not None:
-            cat_name = cat.get('name')
-            item_name = t.get('name')
-            if t.find('nominal').text != '0':
-                if cat_name == 'weapons':
-                    weapons.items.append(trader.Weapon(item_name, 0, 0))
-                elif cat_name == 'containers':
-                    containers.items.append(trader.Singular(item_name, 0, 0))
-                elif cat_name == 'clothes':
-                    clothes.items.append(trader.Singular(item_name, 0, 0))
-                elif cat_name == 'explosives':
-                    explosives.items.append(trader.Item(item_name, 1, 0, 0))
-                elif cat_name == 'food':
-                    food.items.append(trader.Item(item_name, 1, 0, 0))
-                elif cat_name == 'tools':
-                    tools.items.append(trader.Singular(item_name, 0, 0))
-
-    for v in ('OffroadHatchback',
-              'OffroadHatchback_Blue',
-              'OffroadHatchback_White',
-              'Hatchback_02',
-              'Hatchback_02_Blue',
-              'Hatchback_02_Black',
-              'Sedan_02',
-              'Sedan_02_Red',
-              'Sedan_02_Grey',
-              'CivilianSedan',
-              'CivilianSedan_Wine',
-              'CivilianSedan_Black',
-              'CrSk_BMW_525i_E34_black'):
-        vehicles.items.append(trader.Vehicle(v, 0, 0))
-
-    food_trader = trader.Trader('Food')
-    tool_trader = trader.Trader('Tools')
-    weapon_trader = trader.Trader('Weapons')
-    accessories_trader = trader.Trader('Accessories')
-    clothing_trader = trader.Trader('Clothing')
-    vehicles_trader = trader.Trader('Vehicles')
-
-    food_trader.categories = [food]
-    tool_trader.categories = [tools]
-    weapon_trader.categories = [weapons, explosives]
-    accessories_trader.categories = []
-    clothing_trader.categories = [clothes]
-    vehicles_trader.categories = [containers, vehicles]
-    # Order of traders must match markers in trader_locations.json
-    traders_config.traders += [vehicles_trader, clothing_trader, food_trader, weapon_trader, accessories_trader,
-                               tool_trader]
