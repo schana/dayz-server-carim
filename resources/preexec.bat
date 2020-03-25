2ce21da03ed1f267ea8d099938e4f86d1137f952
Nathaniel Schaaf <schaafna@gmail.com>
2020-03-25 23:18:21 +0100

    Fix batch syntax error

--- resources/preexec.bat
+++ resources/preexec.bat
@@ -2,7 +2,7 @@ SET appdir=.\path\to\dayz-server-carim
 SET deploydir=.\path\to\DayZServer\deploy
 SET authfile=.\path\to\auth.json
 SET outdir=.\path\to\DayZServer
-for /f %%i in ('powershell -c "get-date -format yyyy-MM-dd-HH:mm:ss"') do @set datetime=%%i
+for /f %%i in ('powershell -c "get-date -format yyyy-MM-dd-HHmmss"') do @set datetime=%%i
 SET logfile=.\path\to\preexec_%datetime%.log
 SET "cmd=Python3.exe -m carim.main -d %deploydir% -a %authfile% -o %outdir%"
 call :log >>%logfile% 2>&1
