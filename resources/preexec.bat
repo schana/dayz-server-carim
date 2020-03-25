9da4fcddf26e75687e29f9466a847f70c7c8323d
Nathaniel Schaaf <schaafna@gmail.com>
2020-03-25 10:08:48 +0100

    Update preexec script to create new logfiles with timestamp for each run

--- resources/preexec.bat
+++ resources/preexec.bat
@@ -2,7 +2,8 @@ SET appdir=.\path\to\dayz-server-carim
 SET deploydir=.\path\to\DayZServer\deploy
 SET authfile=.\path\to\auth.json
 SET outdir=.\path\to\DayZServer
-SET logfile=.\path\to\preexec.log
+for /f %%i in ('powershell -c "get-date -format yyyy-MM-dd-HH:mm:ss"') do @set datetime=%%i
+SET logfile=.\path\to\preexec_%datetime%.log
 SET "cmd=Python3.exe -m carim.main -d %deploydir% -a %authfile% -o %outdir%"
 call :log >>%logfile% 2>&1
 exit /b
