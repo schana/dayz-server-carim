79a9d66d765174c990fdc6047ae458a521b6932e
Nathaniel Schaaf <schaafna@gmail.com>
2020-05-04 10:08:52 +0200

    Update advertisement script

--- advertisements/generate.py
+++ advertisements/generate.py
@@ -1,32 +1,11 @@
-advert = """
-{bold_open}Carim{bold_close} Loot+|Guns+|NPCs+|Airdrops|Trader|SimpleBase|Autorun
-178.63.43.16:2302
+advert = """{bold_open}Carim{bold_close} Loot+|Guns+|NPCs+|Airdrops|Trader|SimpleBase|Autorun
+`178.63.43.16:2302`
 
 Come join Carim, the server dedicated to serving the community through open source contributions. The server's configuration is automatically generated to be fun, fair, and balanced for whatever playstyle suits you. The server is managed by an IT professional, ensuring stability and responsiveness to issues.
 
 {open_name}Full mod list{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2034121973{close_link}
 
-Open source tools:
-
-1. {open_name}Carim Discord Bot{close_name}{open_link}https://github.com/schana/carim-discord-bot{close_link} a bot that provides cross chat between the game and discord along with admin RCon commands for server owners
-2. {open_name}Carim configuration tool{close_name}{open_link}https://github.com/schana/dayz-server-carim{close_link} tool used to generate the economy and mod configuration
-
-Open source mods:
-
-1. {open_name}SchanaModAutorun{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2031792120{close_link}
-2. {open_name}SchanaModCompass{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2034121973{close_link}
-3. {open_name}SchanaModTraderNametags{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2034121973{close_link}
-4. {open_name}SchanaModLockPick{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2063133049{close_link}
-5. {open_name}SchanaModNoVehicleDamage{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2022423344{close_link}
-6. {open_name}SchanaModSurvivorSelect{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2037681580{close_link}
-7. {open_name}SchanaModExpansionChatRCon{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2061823484{close_link}
-8. {open_name}SchanaModAdminOmniTool{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2022600094{close_link}
-
-In Development:
-
-1. {open_name}SchanaModParty{close_name}{open_link}https://steamcommunity.com/sharedfiles/filedetails/?id=2071036853{close_link}
-
-Come support growing the community through open source!
+Come support growing the community through open source! More info can be found on {open_name}the website{close_name}{open_link}https://schana.github.io/carim/{close_link}.
 
 {open_name}Carim Discord{close_name}{discord_open_link}https://discord.gg/kdPnVu4{discord_close_link}
 """
