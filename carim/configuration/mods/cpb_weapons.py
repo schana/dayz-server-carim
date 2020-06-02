08a279adb5bc10c10538ee53d84d4fa1953cd5aa
Nathaniel Schaaf <schaafna@gmail.com>
2020-06-03 00:06:30 +0200

    Fix CPB import tiers

--- carim/configuration/mods/cpb_weapons.py
+++ carim/configuration/mods/cpb_weapons.py
@@ -10,4 +10,11 @@ from carim.global_resources import types
 def items_cpb_weapons():
     with open('resources/original-mod-files/CPBWeapons/types(NOT A REPLACE).xml') as f:
         new_types = ElementTree.fromstring(f.read())
+
+    for new_type in new_types:
+        for value in new_type.findall('.//value'):
+            new_type.remove(value)
+        ElementTree.SubElement(new_type, 'value', attrib=dict(name='Tier4'))
+        ElementTree.SubElement(new_type, 'value', attrib=dict(name='Tier3'))
+
     types.get().getroot().extend(new_types)
